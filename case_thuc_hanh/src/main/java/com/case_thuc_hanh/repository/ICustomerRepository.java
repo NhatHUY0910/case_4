2ad2f2291e218495241d63614647384ecc441d92
NhatHUY0910 <157088439+NhatHUY0910@users.noreply.github.com>
2024-06-12 11:40:56 +0700

    Delete case_thuc_hanh directory

--- case_thuc_hanh/src/main/java/com/case_thuc_hanh/repository/ICustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.case_thuc_hanh.repository;
-
-import com.case_thuc_hanh.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICustomerRepository extends JpaRepository<Customer, Long> {
-}
--- case_thuc_hanh/src/main/java/com/case_thuc_hanh/repository/ITransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.case_thuc_hanh.repository;
-
-import com.case_thuc_hanh.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ITransactionRepository extends JpaRepository<Transaction, Long> {
-}
